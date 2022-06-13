3dbc7c24b0c144c64ce5265c9e486ea978ce771b
rokylo <rokas.beniusis@ktu.edu>
2022-06-14 00:07:50 +0300

    some changes

--- KlausimynasLAM/Assets/Scripts/AnswerScript.cs
+++ KlausimynasLAM/Assets/Scripts/AnswerScript.cs
@@ -58,32 +58,44 @@ public class AnswerScript : MonoBehaviour
 
     public void ClickFirst()
     {
-        Opt1.GetComponent<Image>().color = Color.gray;
+        Opt1.GetComponent<Image>().color = new Color32(83, 68, 197, 255);
+        //Opt1.GetComponent<Image>().color = Color.gray;
+        Opt1.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = new Color32(16, 16, 56, 255);
         Opt2.GetComponent<Image>().color = Color.white;
         Opt3.GetComponent<Image>().color = Color.white;
         firstClicked=true;
         Opt2.GetComponent<AnswerScript>().secondClicked=false;
         Opt3.GetComponent<AnswerScript>().thirdClicked=false;
+        Opt2.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = Color.white;
+        Opt3.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = Color.white;
     }
 
     public void ClickSecond()
     {
         Opt1.GetComponent<Image>().color = Color.white;
-        Opt2.GetComponent<Image>().color = Color.gray;
+        //Opt2.GetComponent<Image>().color = Color.gray;
+        Opt2.GetComponent<Image>().color = new Color32(83, 68, 197, 255);
+        Opt2.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = new Color32(16, 16, 56, 255);
         Opt3.GetComponent<Image>().color = Color.white;
         Opt1.GetComponent<AnswerScript>().firstClicked=false;
         secondClicked=true;
         Opt3.GetComponent<AnswerScript>().thirdClicked=false;
+        Opt1.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = Color.white;
+        Opt3.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = Color.white;
     }
 
     public void ClickThird()
     {
         Opt1.GetComponent<Image>().color = Color.white;
         Opt2.GetComponent<Image>().color = Color.white;
-        Opt3.GetComponent<Image>().color = Color.gray;
+        //Opt3.GetComponent<Image>().color = Color.gray;
+        Opt3.GetComponent<Image>().color = new Color32(83, 68, 197, 255);
+        Opt3.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = new Color32(16, 16, 56, 255);
         Opt1.GetComponent<AnswerScript>().firstClicked=false;
         Opt2.GetComponent<AnswerScript>().secondClicked=false;
         thirdClicked=true;
+        Opt1.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = Color.white;
+        Opt2.transform.GetChild(0).transform.GetChild(1).GetComponent<Image>().color = Color.white;
     }
 
     public void RestoreBools()
